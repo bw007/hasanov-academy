28ac2ac5b7fa9fcb88e469ae5c3bda911ac185c9
bw007 <mr.khasanov02@gmail.com>
2025-09-23 15:53:47 +0500

    update: fix imports

---
+++ src/app/layouts/main-layout/main-layout.ts
@@ -0,0 +1,35 @@
+import { Component, computed, inject } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { View } from "@core/enums";
+
+import { Navigation, Responsive } from "@core/services";
+import { BottomNav, Header } from "@shared/components";
+
+@Component({
+  selector: "app-main-layout",
+  templateUrl: "./main-layout.html",
+  imports: [Header, BottomNav, RouterOutlet]
+})
+export class MainLayout {
+  private responsive = inject(Responsive);
+  private navigation = inject(Navigation);
+
+  currentView = computed(() => {
+    if (this.responsive.isMobile()) return View.Mobile;
+    if (this.responsive.isTablet()) return View.Tablet;
+    return View.Desktop;
+  });
+
+  getNavsTopSecondary = computed(() => this.navigation.getTopSecondaryItems());
+
+  getNavsCurrentBreakpoint = computed(() => {
+    const view = this.currentView();
+
+    switch (view) {
+      case View.Desktop:
+        return this.navigation.getTopPrimaryItems();
+      default:
+        return this.navigation.getBottomNavItems();        
+    }
+  });
+}
