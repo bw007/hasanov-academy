be1f8afd5ceeb76fe2bc5206bcdf2c2792451d17
bw007 <mr.khasanov02@gmail.com>
2025-09-23 13:19:29 +0500

    feat: refactor Responsive service to use signals for breakpoint detection

--- src/app/core/services/responsive.ts
+++ src/app/core/services/responsive.ts
@@ -1,13 +1,20 @@
-import { inject, Injectable } from "@angular/core";
+import { computed, inject, Injectable } from "@angular/core";
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { toSignal } from "@angular/core/rxjs-interop";
 
 @Injectable({ providedIn: 'root' })
-export class ResponsiveService {
-  breakpointObserver$ = inject(BreakpointObserver);
+export class Responsive {
+  private breakpointObserver = inject(BreakpointObserver);
 
-  isHandset$ = this.breakpointObserver$.observe([Breakpoints.XSmall]);
-  isTablet$ = this.breakpointObserver$.observe([Breakpoints.Tablet]);
-  isWeb$ = this.breakpointObserver$.observe([Breakpoints.Web]);
+  private handset$ = this.breakpointObserver.observe([Breakpoints.XSmall]);
+  private tablet$ = this.breakpointObserver.observe(['(min-width: 600px) and (max-width: 839.98px)']);
+  private web$ = this.breakpointObserver.observe(['(min-width: 840px) and (max-width: 1279.98px)']);
 
-  isHandsetOrTablet$ = this.breakpointObserver$.observe('(max-width: 959px)');
+  handset = toSignal(this.handset$, { initialValue: { matches: false, breakpoints: {} } });
+  tablet = toSignal(this.tablet$, { initialValue: { matches: false, breakpoints: {} } });
+  web = toSignal(this.web$);
+
+  isMobile = computed(() => this.handset()?.matches ?? false);
+  isTablet = computed(() => this.tablet()?.matches ?? false);
+  isDesktop = computed(() => this.web()?.matches ?? false);
 }
