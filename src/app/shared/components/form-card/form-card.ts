aef39ddc601093770fb235cd3a18b7a5cd5a0a86
bw007 <mr.khasanov02@gmail.com>
2025-09-27 21:18:49 +0500

    feat: add reusable form-card component

---
+++ src/app/shared/components/form-card/form-card.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { CardModule } from "primeng/card";
+
+@Component({
+  selector: "app-form-card",
+  templateUrl: "./form-card.html",
+  imports: [CardModule],
+})
+export class FormCard {}
