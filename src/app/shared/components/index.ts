5e34256fff51b42b07cbd21a7108fcf64aba0af5
bw007 <mr.khasanov02@gmail.com>
2025-09-27 21:23:01 +0500

    feat(shared): export FormCard and OtpVerification

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,2 +1,4 @@
 export { Header } from './header/header';
-export { BottomNav } from './bottom-nav/bottom-nav'
+export { BottomNav } from './bottom-nav/bottom-nav';
+export { FormCard } from './form-card/form-card';
+export { OtpVerification } from './otp-verification/otp-verification';
