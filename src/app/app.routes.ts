70c2d8ad2d95ab7459771352178885e0c3a40078
bw007 <mr.khasanov02@gmail.com>
2025-09-27 21:18:10 +0500

    feat(router): add auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,35 @@
 import { Routes } from '@angular/router';
-import { MainLayout } from '@layouts/main-layout/main-layout';
+import { AuthLayout, MainLayout } from './layouts';
+import { SignIn } from '@features/auth/sign-in/sign-in';
+import { SignUp } from '@features/auth/sign-up/sign-up';
+import { VerifyEmail } from '@features/auth/verify-email/verify-email';
+import { ForgotPassword } from '@features/auth/forgot-password/forgot-password';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout
+  },
+  {
+    path: 'auth',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'sign-in',
+        component: SignIn
+      },
+      {
+        path: 'sign-up',
+        component: SignUp
+      },
+      {
+        path: 'verify-email',
+        component: VerifyEmail
+      },
+      {
+        path: 'forgot-password',
+        component: ForgotPassword
+      }
+    ]
   }
 ];
