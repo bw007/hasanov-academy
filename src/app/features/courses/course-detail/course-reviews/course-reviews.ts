76eda538700c0a79f634748d87af19921eda8e85
bw007 <mr.khasanov02@gmail.com>
2025-10-01 20:37:14 +0500

    feat: create course reviews component

---
+++ src/app/features/courses/course-detail/course-reviews/course-reviews.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-course-reviews",
+  templateUrl: "./course-reviews.html",
+  imports: []
+})
+export class CourseReviews {}
