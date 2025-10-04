af0eaa4079a8de1522c24074a0f110757b419f25
bw007 <mr.khasanov02@gmail.com>
2025-10-04 15:18:41 +0500

    feat(routes): add lazy-loaded auth routes

---
+++ src/app/layouts/auth-layout/auth.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from "@angular/router";
+import { SignIn } from "@features/auth/sign-in/sign-in";
+
+export const AUTH_ROUTES: Routes = [
+  { path: "", redirectTo: "sign-in", pathMatch: "full" },
+  { 
+    path: "sign-in", 
+    component: SignIn
+  },
+  { 
+    path: "sign-up", 
+    loadComponent: () => import("@features/auth/sign-up/sign-up").then(m => m.SignUp)
+  },
+  { 
+    path: "verify-email", 
+    loadComponent: () => import("@features/auth/verify-email/verify-email").then(m => m.VerifyEmail)
+  },
+  { 
+    path: "forgot-password", 
+    loadComponent: () => import("@features/auth/forgot-password/forgot-password").then(m => m.ForgotPassword)
+  },
+];
