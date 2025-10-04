0d3a22e5298bb7da3411e813a835bb0ceb70ff5d
bw007 <mr.khasanov02@gmail.com>
2025-10-04 15:21:46 +0500

    refactor(routes): split routes into separate lazy-loaded modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,46 +1,15 @@
 import { Routes } from '@angular/router';
-import { AuthLayout, MainLayout } from './layouts';
-import { SignIn } from '@features/auth/sign-in/sign-in';
-import { SignUp } from '@features/auth/sign-up/sign-up';
-import { VerifyEmail } from '@features/auth/verify-email/verify-email';
-import { ForgotPassword } from '@features/auth/forgot-password/forgot-password';
-import { CoursesList } from '@features/courses/courses-list/courses-list';
-import { CourseDetail } from '@features/courses/course-detail/course-detail';
+import { MainLayout } from './layouts';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
-    children: [
-      {
-        path: 'courses',
-        children: [
-          { path: '', component: CoursesList },
-          { path: ':id', component: CourseDetail }
-        ]
-      }
-    ]
+    loadChildren: () => import('./layouts/main-layout/main.routes').then(m => m.MAIN_ROUTES)
   },
   {
     path: 'auth',
-    component: AuthLayout,
-    children: [
-      {
-        path: 'sign-in',
-        component: SignIn
-      },
-      {
-        path: 'sign-up',
-        component: SignUp
-      },
-      {
-        path: 'verify-email',
-        component: VerifyEmail
-      },
-      {
-        path: 'forgot-password',
-        component: ForgotPassword
-      }
-    ]
+    loadComponent: () => import('./layouts/auth-layout/auth-layout').then(m => m.AuthLayout),
+    loadChildren: () => import('./layouts/auth-layout/auth.routes').then(m => m.AUTH_ROUTES)
   }
 ];
