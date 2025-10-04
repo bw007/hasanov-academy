c53766976229ac05d5a1672fc92b67ecc608a583
bw007 <mr.khasanov02@gmail.com>
2025-10-04 13:46:19 +0500

    update: resolve main layout, add scroller service

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,19 +1,26 @@
-import { Component, computed, inject } from "@angular/core";
+import { Component, computed, DestroyRef, inject, OnInit, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { View } from "@core/enums";
+import { NgClass, ViewportScroller } from "@angular/common";
+import { debounceTime, fromEvent } from "rxjs";
 
 import { Navigation, Responsive } from "@core/services/layout";
+import { View } from "@core/enums";
+
 import { BottomNav } from "./bottom-nav/bottom-nav";
 import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: "app-main-layout",
   templateUrl: "./main-layout.html",
-  imports: [Header, BottomNav, RouterOutlet]
+  imports: [Header, BottomNav, RouterOutlet, NgClass, Footer],
 })
-export class MainLayout {
+export class MainLayout implements OnInit {
+  private dsRef = inject(DestroyRef);
   private responsive = inject(Responsive);
-  private navigation = inject(Navigation);
+  protected readonly navigation = inject(Navigation);
+  private viewportScroller = inject(ViewportScroller);
+  protected readonly view = View;
 
   currentView = computed(() => {
     if (this.responsive.isMobile()) return View.Mobile;
@@ -21,16 +28,30 @@ export class MainLayout {
     return View.Desktop;
   });
 
-  getNavsTopSecondary = computed(() => this.navigation.getTopSecondaryItems());
-
   getNavsCurrentBreakpoint = computed(() => {
-    const view = this.currentView();
-
-    switch (view) {
+    switch (this.currentView()) {
       case View.Desktop:
         return this.navigation.getTopPrimaryItems();
       default:
         return this.navigation.getBottomNavItems();        
     }
   });
+
+  scrollDown = signal(false);
+
+  ngOnInit(): void {
+    const subscription = fromEvent(window, 'scroll')
+      .pipe(debounceTime(100))
+      .subscribe({
+        next: () => {
+          if (this.viewportScroller.getScrollPosition()[1] > 80) {
+            this.scrollDown.set(true);
+          } else {
+            this.scrollDown.set(false);
+          }
+        }
+      });
+    
+    this.dsRef.onDestroy(() => subscription.unsubscribe())
+  };
 }
