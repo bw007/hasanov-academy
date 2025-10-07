54a36fd21222eb7a6c56abfe2f988fa2435aa62b
bw007 <mr.khasanov02@gmail.com>
2025-10-08 00:33:21 +0500

    fix(core): resolve get token

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -3,21 +3,18 @@ import {
   HttpInterceptorFn,
   HttpRequest,
 } from "@angular/common/http";
-import { inject } from "@angular/core";
-import { Auth } from "@core/services/api";
 
 export const authInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) => {
-  const auth = inject(Auth);
+  const accessToken = localStorage.getItem("access_token");
   
-  const accessToken = auth.accessToken();
   const publicEndpoints = ["signin", "signup", "refresh-token", "verify-email"];
   const isPublicEndpoint = publicEndpoints.some(endpoint => 
     req.url.includes(endpoint)
   );
-
+ 
   if (isPublicEndpoint || !accessToken) {
     return next(req);
   }
