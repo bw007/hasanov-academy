af5f2eebe6651fe19d1498a85c9b74c474f8d487
bw007 <mr.khasanov02@gmail.com>
2025-10-07 16:44:42 +0500

    feat(ui): implement primeng toast component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,21 @@
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, computed, inject, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { Theme } from "@core/services/layout";
+import { Responsive, Theme } from "@core/services/layout";
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet />`,
-  imports: [RouterOutlet],
+  template: `
+    <p-toast [breakpoints]="{ '580px': { width: '280px' } }" [position]="toastPosition()" />
+    <router-outlet />
+  `,
+  imports: [RouterOutlet, ToastModule],
 })
 export class App implements OnInit{
   private theme = inject(Theme);
+  protected readonly responsive = inject(Responsive);
+
+  toastPosition = computed(() => this.responsive.isDesktop() ? "top-right" : "top-center");
 
   ngOnInit(): void {
     this.theme.loadTheme();
