840842efc070956dcdd0ab122087d8482b224f2b
bw007 <mr.khasanov02@gmail.com>
2025-10-07 16:20:39 +0500

    chore(config): configure http client and message service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,44 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from "@angular/core";
 
-import { routes } from './app.routes';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+
+import { routes } from "./app.routes";
+import { authInterceptor, httpErrorInterceptor, urlInterceptor } from "@core/interceptors";
+
+import { providePrimeNG } from "primeng/config";
+import Aura from "@primeuix/themes/aura";
+import { MessageService } from "primeng/api";
+
+import { errorMessagesProvider } from "@core/constants/error-messages";
+import { successMessagesProvider } from "@core/constants/success-messages";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([
+      urlInterceptor,
+      authInterceptor,
+      httpErrorInterceptor
+    ])),
+    MessageService,
+    errorMessagesProvider,
+    successMessagesProvider,
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
         options: {
-          darkModeSelector: '.app-dark'
-        }
-      }
-    })
-  ]
+          darkModeSelector: ".app-dark",
+        },
+      },
+    }),
+  ],
 };
