74b1d64d958b0fa955ab5d34c362ff08dfdc8ac7
bw007 <mr.khasanov02@gmail.com>
2025-10-08 23:18:29 +0500

    feat(auth): implement verifyEmail/resendOTP from auth service

--- src/app/features/auth/verify-email/verify-email.ts
+++ src/app/features/auth/verify-email/verify-email.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, signal } from "@angular/core";
+import { Component, DestroyRef, inject, input, signal } from "@angular/core";
 import { Location } from "@angular/common";
 import { Router } from "@angular/router";
 
@@ -7,6 +7,9 @@ import { InputOtpModule } from "primeng/inputotp"
 import { FormsModule } from "@angular/forms";
 
 import { FormCard, OtpVerification } from "@shared/components";
+import { Auth } from "@core/services/api";
+import { catchError, finalize, throwError } from "rxjs";
+import { Notification } from "@core/services/common";
 
 enum verifyStatus {
   Pending = 'pending',
@@ -27,30 +30,56 @@ enum verifyStatus {
 })
 export class VerifyEmail {
   private router = inject(Router);
-  location = inject(Location);
+  private dsRef = inject(DestroyRef);
+  protected readonly location = inject(Location);
+  private auth = inject(Auth);
+  private notification = inject(Notification);
 
-  email = input<string>();
+  email = input.required<string>();
 
   otpVerified = signal<verifyStatus | ''>('');
   isResend = signal(false);
 
   onVerifyOtp(code: string) {
     this.otpVerified.set(verifyStatus.Pending)
-    console.log(code);
-    setTimeout(() => {
-      this.otpVerified.set(verifyStatus.Error);
-    }, 2000);
-    
+    const subscription = this.auth.verifyEmail({
+      email: this.email()!,
+      otp: code!
+    }).pipe(
+      catchError(error => {
+        this.otpVerified.set(verifyStatus.Error);
+        return throwError(() => error);
+      })
+    ).subscribe({
+      next: () => {
+        this.otpVerified.set(verifyStatus.Success);
+        this.notification.success({
+          summary: 'Qoyilmaqom!',
+          message: 'Muvaffaqiyatli tizimga kirdingiz.'
+        });
+        this.router.navigateByUrl('/')
+      }
+    });
+
+    this.dsRef.onDestroy(() => subscription.unsubscribe());
   }
 
   onResenCode() {
     this.isResend.set(true);
-    setTimeout(() => {
-      this.isResend.set(false)
-    }, 2000);
-  }
+    const subscription = this.auth.resendOTP(this.email())
+      .pipe(
+        finalize(() => {
+          this.isResend.set(false);
+        })
+      ).subscribe({
+        next: () => {
+          this.notification.success({
+            summary: 'Yuborildi',
+            message: 'E-pochtani ko\'zdan kechiring'
+          })
+        }  
+      });
 
-  signIn() {
-    
+    this.dsRef.onDestroy(() => subscription.unsubscribe());
   }
 }
