7ceec03d06960f549fcf092cd0cf0370717fa54e
bw007 <mr.khasanov02@gmail.com>
2025-10-08 23:30:20 +0500

    feat(auth): Implement async email validation, signup, and notification service

--- src/app/features/auth/sign-up/sign-up.ts
+++ src/app/features/auth/sign-up/sign-up.ts
@@ -2,7 +2,11 @@ import { Component, computed, DestroyRef, ElementRef, inject, OnInit, signal, vi
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Router, RouterLink } from "@angular/router";
 import { toSignal } from "@angular/core/rxjs-interop";
-import { debounceTime, delay, map, of, startWith } from "rxjs";
+import { debounceTime, delay, finalize, map, of, startWith, switchMap } from "rxjs";
+
+import { Auth } from "@core/services/api";
+import { Notification } from "@core/services/common";
+import { AuthType } from "@core/models";
 
 import { ButtonModule } from "primeng/button";
 import { InputTextModule } from "primeng/inputtext";
@@ -11,39 +15,16 @@ import { AutoFocusModule } from "primeng/autofocus";
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { PasswordModule } from 'primeng/password';
 import { MessageModule } from "primeng/message";
-
 import { FormCard } from "@shared/components";
 
-let initialNameValue = '', initialEmailValue = '', initialPass = '';
+let initialNameValue = '', initialEmailValue = '';
 const savedForm = window.localStorage.getItem('sign-up-form');
 
 if (savedForm) {
   const loadedForm = JSON.parse(savedForm);
   initialNameValue = loadedForm.name;
   initialEmailValue = loadedForm.email;
-  initialPass = loadedForm.passwords.password;
 };
-
-function equalPasswords(control:AbstractControl) {
-  const p1 = control.value.password;
-  const p2 = control.value.confirmPassword;
-  
-  if (p1 === p2) return null;
-  return { passwordsNoEqual: true }
-};
-
-function emailIsUnique(control:AbstractControl) {
-  if (!control.value) return of(null);
-  const db = ['test@gmail.com', 'test1@mail.ru'];
-
-  return of(null).pipe(
-    delay(3000),
-    map(() => {
-      if (!db.includes(control.value)) return null;
-      return { emailTaken: true }
-    })
-  )
-}
 
 @Component({
   selector: "app-sign-up",
@@ -63,27 +44,29 @@ function emailIsUnique(control:AbstractControl) {
 ],
 })
 export class SignUp implements OnInit{
-  private destroyRef = inject(DestroyRef);
+  private dsRef = inject(DestroyRef);
   private router = inject(Router);
+  private auth = inject(Auth);
+  private notification = inject(Notification);
 
   form = new FormGroup({
     name: new FormControl(initialNameValue, {
       validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
     }),
     email: new FormControl(initialEmailValue, {
-      validators: [Validators.required, Validators.email],
-      asyncValidators: [emailIsUnique],
+      validators: [Validators.required, Validators.email, Validators.pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)],
+      asyncValidators: [(control) => this.emailIsUnique(control)],
       updateOn: 'blur'
     }),
     passwords: new FormGroup({
-      password: new FormControl(initialPass, {
+      password: new FormControl('', {
         validators: [Validators.required, Validators.minLength(8)]
       }),
-      confirmPassword: new FormControl(initialPass, {
+      confirmPassword: new FormControl('', {
         validators: [Validators.required]
       })
     }, {
-      validators: [equalPasswords]
+      validators: [this.equalPasswords]
     })
   });
 
@@ -118,7 +101,7 @@ export class SignUp implements OnInit{
     if (!control.touched || !control.dirty || control.valid) return null;
     
     if (control.hasError('required')) return 'Majburiy maydon';
-    if (control.hasError('email')) return 'Noto\'g\'ri e-pochta shakli';
+    if (control.hasError('email') || control.hasError('pattern')) return 'Noto\'g\'ri e-pochta shakli';
     if (control.hasError('emailTaken')) return 'Bu email allaqachon ro\'yxatdan o\'tgan';
     return null;
   };
@@ -164,46 +147,100 @@ export class SignUp implements OnInit{
       }
     });
 
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    this.dsRef.onDestroy(() => subscription.unsubscribe());
   };
+
+  private equalPasswords(control:AbstractControl) {
+    const p1 = control.value.password;
+    const p2 = control.value.confirmPassword;
+    
+    if (p1 === p2) return null;
+    return { passwordsNoEqual: true }
+  };
+
+  private emailIsUnique(control: AbstractControl) {
+    if (!control.value) return of(null);
+
+    return of(null).pipe(
+      delay(500),
+      switchMap(() => this.auth.checkEmail(control.value)),
+      map((isAvailable: boolean) => {          
+        if (!isAvailable) {
+          return { emailTaken: true };
+        }
+          
+        return null;
+      })
+    );
+  }
 
   signUp(event: Event) {
-    if (this.form.invalid && event.type === 'submit') {
+    const eventType = event.type;
+
+    // Google signin
+    if (eventType === AuthType.Google) {
+      this.signInWithGoogle();
+      return;
+    }
+
+    // Form submit
+    if (eventType === AuthType.Email) {
+      this.signUpWithEmail();
+    }
+  }
+  
+  private signUpWithEmail() {   
+    // Form validation
+    if (this.form.invalid) {
       this.form.markAllAsTouched();
       this.form.markAllAsDirty();
-
-      const invalidInput = this.formInputs().find(input => 
+      
+      const invalidInput = this.formInputs().find(input =>
         input.nativeElement.classList.contains('ng-invalid')
       );
-
-      if (invalidInput) {
-        invalidInput.nativeElement.focus();
-      }
+      
+      invalidInput?.nativeElement.focus();
       return;
-    };
+    }
 
-    this.onValid(event.type);
-
-    if (this.form.valid && event.type === 'submit') {
-      setTimeout(() => {
+    // Sign in
+    this.loadingState.set({ loading: true, type: AuthType.Email });
+    const email = this.form.value.email!;
+    const subscription = this.auth.signUp({
+      email,
+      name: this.form.value.name!,
+      password: this.form.value.passwords?.password!
+    }).pipe(
+      finalize(() => {
         this.loadingState.set({ loading: false, type: '' });
-        console.log(111111111111);
-        
-        this.router.navigate(['/auth/verify-email'], {
-          queryParams: { email: this.form.controls.email.value }
+      })
+    ).subscribe({
+      next: () => {
+        this.notification.success({
+          summary: 'Qoyilmaqom, siz zo\'rsiz!',
+          message: 'E-pochtani tasdiqlang va tizimga kiring.'
         });
-        // this.onReset();
-      }, 3000);
-    };
+        this.router.navigate(['auth/verify-email'], {
+          queryParams: { email }
+        });
+      }
+    });
 
-    if (event.type === 'click') {
-      setTimeout(() => {
-        this.loadingState.set({ loading: false, type: '' });
-        this.router.navigate(['/']);
-        // this.onReset();
-      }, 3000);
-    };
-  };
+    this.dsRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  private signInWithGoogle() {
+    this.loadingState.set({ loading: true, type: AuthType.Google });
+    this.form.markAsPristine();
+    // TODO: Implement Google OAuth
+    this.notification.info({
+      summary: 'Tez orada!',
+      message: 'Yaqin vaqtlar ichida ushbu xizmat qo\'shiladi'
+    });
+    setTimeout(() => {
+      this.loadingState.set({ loading: false, type: '' });
+    }, 1000);
+  }
 
   onReset() {
     this.form.reset();
