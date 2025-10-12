1c99214230efcde704684138feac2a6ce2bf2350
bw007 <mr.khasanov02@gmail.com>
2025-10-13 04:39:12 +0500

    feat: add search panel

--- src/app/features/courses/courses-list/search-bar/search-bar.ts
+++ src/app/features/courses/courses-list/search-bar/search-bar.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, ElementRef, inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, fromEvent, map, switchMap } from 'rxjs';
+import { Course } from '@core/services/api';
 
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-search-bar',
     templateUrl: './search-bar.html',
     imports: [FormsModule, InputIconModule, IconFieldModule, InputTextModule],
   })
-export class SearchBar {}
+export class SearchBar implements AfterViewInit {
+  private dsRef = inject(DestroyRef);
+  private course = inject(Course);
+  searchInput = viewChild.required<ElementRef>('searchInput');
+
+  ngAfterViewInit (): void {
+    fromEvent<InputEvent>(this.searchInput().nativeElement, "input")
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        map((event) => (event.target as HTMLInputElement).value.trim()),
+        switchMap((value) => this.course.getAllCourses(value)),
+        takeUntilDestroyed(this.dsRef)
+      )
+      .subscribe();
+  }
+}
