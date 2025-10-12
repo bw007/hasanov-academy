175059bbd1d900915ca79645fad9e1bde93ea90d
bw007 <mr.khasanov02@gmail.com>
2025-10-13 04:41:47 +0500

    feat: add barrel exports

--- src/app/core/constants/index.ts
+++ src/app/core/constants/index.ts
@@ -1,2 +1,4 @@
 export { ERROR_MESSAGES } from './error-messages';
-export { SUCCESS_MESSAGES } from './success-messages'
+export { SUCCESS_MESSAGES } from './success-messages';
+export { courseCategory } from "./course-category";
+export { NAV_ITEMS } from "./navigation-items"
---
+++ src/app/core/guards/index.ts
@@ -0,0 +1,2 @@
+export { SignOutGuard, AuthGuard } from "./auth-guard"
+export { StudentGuard } from "./role-guard"
