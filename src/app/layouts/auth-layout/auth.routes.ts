62c0a8ac3e2be87153553471b186686b2d4ccb33
bw007 <mr.khasanov02@gmail.com>
2025-10-13 04:23:39 +0500

    fix: resolve auth routes with guard

--- src/app/layouts/auth-layout/auth.routes.ts
+++ src/app/layouts/auth-layout/auth.routes.ts
@@ -1,23 +1,33 @@
 import { Routes } from "@angular/router";
+import { AuthGuard, SignOutGuard } from "@core/guards";
 import { SignIn } from "@features/auth/sign-in/sign-in";
 
 export const AUTH_ROUTES: Routes = [
   { path: "", redirectTo: "sign-in", pathMatch: "full" },
   { 
-    path: "sign-in", 
+    path: "sign-in",
+    canActivate: [AuthGuard],
     component: SignIn
   },
   { 
-    path: "sign-up", 
+    path: "sign-up",
+    canActivate: [AuthGuard],
     loadComponent: () => import("@features/auth/sign-up/sign-up").then(m => m.SignUp)
   },
   { 
-    path: "verify-email", 
+    path: "verify-email",
+    canActivate: [AuthGuard],
     loadComponent: () => import("@features/auth/verify-email/verify-email").then(m => m.VerifyEmail)
   },
   { 
     path: "forgot-password",
-    redirectTo: 'sign-in'
-    // loadComponent: () => import("@features/auth/forgot-password/forgot-password").then(m => m.ForgotPassword)
+    redirectTo: 'sign-in',
+    pathMatch: 'full'
   },
+  {
+    path: "sign-out",
+    pathMatch: 'full',
+    canActivate: [SignOutGuard],
+    loadComponent: () => import("@features/auth/sign-out").then(c => c.SignOut)
+  }
 ];
