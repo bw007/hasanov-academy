6736071cd2008cf0bc5dd32e4fb33a94637777ac
bw007 <mr.khasanov02@gmail.com>
2025-10-13 04:08:41 +0500

    feat: add course category

---
+++ src/app/core/constants/course-category.ts
@@ -0,0 +1,8 @@
+export const courseCategory = {
+  development: 'Dasturlash',
+  web_development: 'Web dasturlash',
+  cyber_security: 'Kiberxavfsizlik',
+  ai: 'Ai',
+  ui_ux_design: "UI/UX Dizayn",
+  devops: "Devops"
+}
