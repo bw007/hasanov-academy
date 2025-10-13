ab024916a094b3a756e27c57baf5732ae0f39a78
bw007 <mr.khasanov02@gmail.com>
2025-10-13 18:46:20 +0500

    fest: add routing

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,20 +1,23 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: 'app-home',
   template: `
-    <div class="lg:container mx-auto mt-6 px-4 sm:px-6">
+    <div class="w-full lg:max-w-5xl xl:max-w-7xl mx-auto mt-6 px-4 sm:px-6">
       <h1 class="text-xl mb-6">Bosh sahifa. Tez orada...</h1>
       <p-button
         label="Kurslarni ko'rish"
         severity="primary"
         size="small"
+        routerLink="course"
       />
     </div>
   `,
   imports: [
-    ButtonModule
+    ButtonModule,
+    RouterLink
   ]
 })
 
