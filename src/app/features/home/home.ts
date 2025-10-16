a7ec47d4b04065aa5bdb59cb7b037a69542767d1
Lazizbek Hasanov <146458608+bw007@users.noreply.github.com>
2025-10-16 20:08:49 +0500

    Update home.ts

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -7,6 +7,7 @@ import { ButtonModule } from "primeng/button";
   template: `
     <div class="w-full lg:max-w-5xl xl:max-w-7xl mx-auto mt-6 px-4 sm:px-6">
       <h1 class="text-xl mb-6">Bosh sahifa. Tez orada...</h1>
+      <h1>Iltimos kuting, server uyg'onishi uchun boshlani'shiga ozroq vaqt kerak.</h1>
       <p-button
         label="Kurslarni ko'rish"
         severity="primary"
