29fd080bd6accf643e0947f9f6d60a72f1a5abfc
bw007 <mr.khasanov02@gmail.com>
2025-10-23 23:11:15 +0500

    feat: implement aos animate

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,25 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { ButtonModule } from "primeng/button";
 
+import AOS from 'aos'
+
 @Component({
   selector: 'app-home',
-  template: `
-    <div class="w-full lg:max-w-5xl xl:max-w-7xl mx-auto mt-6 px-4 sm:px-6">
-      <h1 class="text-xl mb-6">Bosh sahifa. Tez orada...</h1>
-      <h1>Iltimos kuting, server uyg'onishi uchun boshlani'shiga ozroq vaqt kerak.</h1>
-      <p-button
-        label="Kurslarni ko'rish"
-        severity="primary"
-        size="small"
-        routerLink="/courses"
-      />
-    </div>
-  `,
+  templateUrl: './home.html',
   imports: [
     ButtonModule,
     RouterLink
   ]
 })
 
-export class Home {}
+export class Home implements OnInit {
+
+  ngOnInit(): void {
+    AOS.init()
+  }
+}
