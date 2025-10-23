aa62fb8cb0210696aa62c344a171324e4ad4c5ea
bw007 <mr.khasanov02@gmail.com>
2025-10-23 22:16:00 +0500

    feat: add debounceTime

--- src/app/features/cart-view/cart-view.ts
+++ src/app/features/cart-view/cart-view.ts
@@ -5,7 +5,7 @@ import { Auth, Cart } from "@core/services/api";
 import { CourseCardComponent } from "@shared/components/course-card/course-card";
 import { ButtonModule } from "primeng/button";
 import { MessageModule } from "primeng/message";
-import { tap } from "rxjs";
+import { debounceTime, tap } from "rxjs";
 
 @Component({
   selector: 'app-cart-view',
@@ -22,7 +22,7 @@ export class CartView implements OnInit {
   private cart = inject(Cart);
   private auth = inject(Auth);
 
-  cartData = linkedSignal(() => this.cart.cartData());
+  cartData = computed(() => this.cart.cartData());
   priceSumm = computed(() => 
     this.cart.cartData()?.reduce((acc: number, item: any) => acc + item.price, 0)
   );
@@ -31,13 +31,13 @@ export class CartView implements OnInit {
   error = this.cart.error;
 
   ngOnInit(): void {
-    this.cart.getAllCartData().pipe(takeUntilDestroyed(this.dsRef)).subscribe();
+    this.cart.getAllCartData().pipe(debounceTime(500), takeUntilDestroyed(this.dsRef)).subscribe();
   }
 
   removeFromCart(id: string) {
     this.cart.removeCartItem(id)
       .pipe(
-        tap(res => {
+        tap(_ => {
           this.auth.verifyUser().pipe(takeUntilDestroyed(this.dsRef)).subscribe();
         }),
         takeUntilDestroyed(this.dsRef)
